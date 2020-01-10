4063de91463c558b58d2bb27c1a991f7d2a9fbaa
Aaren Chiou <wowchiou@gmail.com>
2020-01-10 15:54:20 +0800

    Updates

---
+++ precache-manifest.548383384f19422c5e7d272ce52a30d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e7fd3c6e882b8bf97b2fc9d0d511b3",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "271de01c4963a9751585",
+    "url": "/react-todo/static/css/main.b2bb3afc.chunk.css"
+  },
+  {
+    "revision": "281ce72589e90a16a6e4",
+    "url": "/react-todo/static/js/2.1839c0e8.chunk.js"
+  },
+  {
+    "revision": "271de01c4963a9751585",
+    "url": "/react-todo/static/js/main.6cff13d3.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
